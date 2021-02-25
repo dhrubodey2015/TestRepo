ea1638f35d9bfe7b6e022af7b82eae75497ef65e
Satyajit Dey <satyajit.dey753@gmail.com>
2021-02-26 02:59:54 +0530

    API Works

--- application/config/aws_sdk.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['aws_access_key']=$_SERVER['AWS_ACCESS_KEY'];
-$config['aws_secret_key']=$_SERVER['AWS_SECRET_KEY'];
